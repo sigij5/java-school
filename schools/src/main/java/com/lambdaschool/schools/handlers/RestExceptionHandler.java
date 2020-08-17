e0cb0238d5aaffc1b9961574341cdfd8ec70635a
Sigi Jaeckel <sigij55@gmail.com>
2020-08-17 19:43:19 -0400

    updated

--- schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handlers/RestExceptionHandler.java
@@ -32,7 +32,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errorDetail.setTimestamp(new Date());
         errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
         errorDetail.setTitle("Resource Not Found");
-        errorDetail.setDetail(rnfe.getMessage());
+        errorDetail.setDetail("Found an error with School: " + rnfe.getMessage());
         errorDetail.setDeveloperMessage(rnfe.getClass().getName());
 
         errorDetail.setErrors(helperFunctions.getConstraintViolation(rnfe));
@@ -46,7 +46,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errorDetail.setTimestamp(new Date());
         errorDetail.setStatus(status.value());
         errorDetail.setTitle("Rest Internal Exception");
-        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDetail("Found an error with School: " + ex.getMessage());
         errorDetail.setDeveloperMessage(ex.getClass().getName());
 
         errorDetail.setErrors(helperFunctions.getConstraintViolation(ex));
